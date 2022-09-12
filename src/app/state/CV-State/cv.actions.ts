b7885c3eeea81ae6ecc900fff107d02382c44de2
onkar-date <onkar101197@gmail.com>
2022-09-12 11:22:26 +0530

    hidden mock data added

--- src/app/state/CV-State/cv.actions.ts
+++ src/app/state/CV-State/cv.actions.ts
@@ -107,3 +107,5 @@ export const removeInterest = createAction(
   '[Interests] Remove Interest',
   props<{ interest: string }>()
 );
+
+export const fillMockData = createAction('[Mock] Fill Mock Data');
